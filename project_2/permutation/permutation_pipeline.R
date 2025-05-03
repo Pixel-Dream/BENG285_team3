1b08ae1ecad228657bc43747feef456036f43fc3
PixelDream <pixeldream@outlook.com>
2025-05-02 17:48:49 -0700

    Update permutation_pipeline.R

--- project_2/permutation/permutation_pipeline.R
+++ project_2/permutation/permutation_pipeline.R
@@ -1,6 +1,17 @@
 library(tidyverse)
 library(biomaRt)
-mutations = read_table("/volume/data/BENG285/TCGA.LUAD.fully_annotated.txt")
+library(pbapply)
+library(parallel)
+source("/volume/data/BENG285/permutation_function.R")
+
+args = commandArgs(trailingOnly=TRUE)
+mutations = args[1]
+#mutations = read_table("/volume/data/BENG285/TCGA.LUAD.fully_annotated.txt")
+outPath = args[2]
+numSims = args[3]
+if(is.null(mutations)) mutations = read_table("/volume/data/BENG285/TCGA.LUAD.fully_annotated.txt")
+if(is.null(outPath)) outPath = "/volume/data/BENG285/"
+if(is.null(numSims)) numSims = 2000
 
 gene_df <- mutations %>% group_by(Hugo_Symbol, Transcript_ID) %>% 
   summarise(n=n())
@@ -66,7 +77,7 @@ simulate_mutations_wrapper <- function(i) {
   cdna <- trx_info_df$cdna[i]
   
   tmp <- estimate_random_mutation_effects(cdna, trx_info_df$n_mutation[i]+trx_info_df$s_mutation[i], 
-                                          num_rounds = 2000, dN = trx_info_df$n_mutation[i],dS = trx_info_df$s_mutation[i])
+                                          num_rounds = numSims, dN = trx_info_df$n_mutation[i],dS = trx_info_df$s_mutation[i])
   # Return results
   return(tmp)
 }
@@ -81,13 +92,10 @@ trx_info_df$n_mutation_est <- permutation_df$average_non_synonymous
 trx_info_df$s_mutation_est <- permutation_df$average_synonymous
 trx_info_df <- left_join(trx_info_df,gene_df[,1:2],by = c("ensembl_transcript_id" = "Transcript_ID"))
 
-write.csv(trx_info_df[,3:9],"/volume/data/BENG285/TCGA_LUAD_permutation_dN_dS_2000.csv")
+
 
 trx_info_df$p_val <- permutation_df$quantile
 trx_info_df$p_adj <- p.adjust(trx_info_df$p_val, method = "fdr")
 sum(trx_info_df$p_adj<0.05)
-
-write.csv(trx_info_df[trx_info_df$p_adj<0.05,3:11],"/volume/data/BENG285/TCGA_LUAD_permutation_dN_dS_2000_subset.csv")
-
-
-
+write.csv(trx_info_df[,3:11],file.path(outPath,paste0("permutation_res_",numSims,".csv")))
+write.csv(trx_info_df[trx_info_df$p_adj<0.05,3:11],file.path(outPath,paste0("permutation_res_",numSims,"_subset.csv")))
