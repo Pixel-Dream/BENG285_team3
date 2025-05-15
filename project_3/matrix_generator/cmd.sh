289ee9f29f1099a5f31e18f20766a81db98a2667
Banghua Xu <xbh0403@gmail.com>
2025-05-14 23:29:53 -0700

    Matrix generator with FILTER

--- project_3/matrix_generator/cmd.sh
+++ project_3/matrix_generator/cmd.sh
@@ -1,1 +1,10 @@
-python generate_sbs_matrix.py --vcf_dir /Users/xbh0403/Desktop/25SP/BENG285/BENG285_team3/project_3/data/vcf_input --output /Users/xbh0403/Desktop/25SP/BENG285/BENG285_team3/project_3/output/sbs_matrix.csv
+#!/bin/bash
+
+# Define paths
+MAF_FILE="/Users/xbh0403/Desktop/25SP/BENG285/BENG285_team3/project_2/data/TCGA.LUAD.mutations.txt"
+OUTPUT_FILE="/Users/xbh0403/Desktop/25SP/BENG285/BENG285_team3/project_3/output/sbs_96_matrix.all"
+
+# Generate SBS matrix
+python generate_sbs_matrix.py \
+    --maf_file /Users/xbh0403/Desktop/25SP/BENG285/BENG285_team3/project_2/data/TCGA.LUAD.mutations.txt \
+    --output /Users/xbh0403/Desktop/25SP/BENG285/BENG285_team3/project_3/output/sbs_96_matrix.all
